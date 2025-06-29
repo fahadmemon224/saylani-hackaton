68fe107d7c3abd187ba3c05726d826895445c6b5
fahadmemon224 <fahadmemon956@gmail.com>
2025-06-29 16:30:53 +0500

    test

--- dialogflow.js
+++ dialogflow.js
@@ -7,7 +7,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID || '';
 const authToken = process.env.TWILIO_AUTH_TOKEN || '';
 const client = require('twilio')(accountSid, authToken);
 
-const cors = require("cors");
+const cors = require("cors"); 
 
 
 const app = express();
